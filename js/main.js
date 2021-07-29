39374e800fd2fcd8d0eba711978fcf0a73fb5faf
MaxservSV <78587688+MaxservSV@users.noreply.github.com>
2021-07-29 18:55:18 +0400

    Адаптив для страницы товара
    Адаптив для страницы товара

--- js/main.js
+++ js/main.js
@@ -15,6 +15,10 @@ var swiper2 = new Swiper(".mySwiper2", {
 
 const menuBtn = document.querySelector(".menu-button")
 const menu = document.querySelector(".navbar-panel")
+const menuCloseBtn = document.querySelector(".close-menu")
 menuBtn.addEventListener('click', ()=>{
+  menu.classList.toggle('is-open')
+})
+menuCloseBtn.addEventListener('click', ()=>{
   menu.classList.toggle('is-open')
 })
